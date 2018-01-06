09487ecdd2c7cc6c68df408073e47c414edcfa72
ZhangXixi <1002500902@qq.com>
2018-01-06 16:31:12 +0800

    git commit

---
+++ src/components/Pos.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pos">
+    Hello AwesomePos Demo
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pos"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
